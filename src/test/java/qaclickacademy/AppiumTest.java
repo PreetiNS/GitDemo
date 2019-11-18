3d1b406f13a562287ae4f22f8b2a9be30406622b
PreetiNS <preethi.ns@gmail.com>
2019-11-18 14:23:49 +0530

    Initial Framework

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,22 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+//Integrate TestNG with Maven and control execution from maven
+//Info: https://maven.apache.org/surefire/maven-surefire-plugin/examples/testng.html 
+//Using Suite XML Files:add the configuration file of TestNG .xml in pom.xml
+
+public class AppiumTest {
+	
+	@Test
+	public void nativeAppAndroid()
+	{
+		System.out.println("nativeAppAndroid");
+	}
+	@Test
+	public void iOSapps()
+	{
+		System.out.println("iOSapps");
+	}
+
+}
